fa148d4e769b01d1e498996dd7145f1daac3d8af
dktmody <diakhite.mody94@gmail.com>
2023-03-22 15:58:56 +0100

    delete redondante variable

--- src/Jeu.java
+++ src/Jeu.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Jeu {
     public static void lancerJeu(){
         char commenceJeux = 'o';
-        char saisiUsers= 'n';
+        char saisiUsers;
         int result;
         System.out.println("Quel est le nom du joueur 1 ");
         Scanner scanner = new Scanner(System.in);
@@ -18,7 +18,6 @@ public class Jeu {
             System.out.println("o pour oui, n pour non!");
             saisiUsers = scanner.next().charAt(0);
             result = Character.compare(commenceJeux, saisiUsers);
-
         }while(result == 0);
 
         if (joueur1.getScore() > joueur2.getScore()){
@@ -29,7 +28,6 @@ public class Jeu {
             System.out.println("match nul, vous avez tous les deux le même nombre de points!");
         }
     }
-
     public static void start(Joueur joueur1, Joueur joueur2){
         Scanner sc = new Scanner(System.in);
         System.out.println("Quel est votre choix ? le tour est pour " + joueur1.getNom() +", pile ou face?");
@@ -38,7 +36,7 @@ public class Jeu {
         System.out.println("Maintenant le tour est pour " + joueur2.getNom());
         choix = sc.nextLine();
         joueur2.setChoix(choix);
-        String lancePileFace = lancerPileFace();
+        String lancePileFace = lancerPiece();
         if (joueur1.getChoix().equals(lancePileFace) ){
             joueur1.setScore(joueur1.getScore() +1);
         }
@@ -46,10 +44,9 @@ public class Jeu {
             joueur2.setScore(joueur2.getScore() +1);
         }
     }
-
-    public static String lancerPileFace(){
-        Piece piecePileFace = new Piece();
-        return piecePileFace.getPiece();
+    public static String lancerPiece(){
+        Piece p = new Piece();
+        return p.getPiece();
     }
     public static void debut(){
         System.out.println("---------------------------------*------------------------------");
@@ -57,5 +54,4 @@ public class Jeu {
         System.out.print("Commencement du jeu pile ou face");
         System.out.println("----------------");
     }
-
 }
--- src/Joueur.java
+++ src/Joueur.java
@@ -30,5 +30,4 @@ public class Joueur {
     public void setScore(int score) {
         this.score = score;
     }
-
 }
